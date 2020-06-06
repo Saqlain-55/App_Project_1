0a4ce2046e669b2fb09cbbbd040b73bf808a445c
Saqlain-55 <Saqlain-55@.github.com>
2020-06-06 23:22:30 +0500

    Update App.js

--- my-react-app/src/App.js
+++ my-react-app/src/App.js
@@ -11,7 +11,7 @@ function App() {
       </h2>
       <p1> Hye! <br/> This is <b>Muhammad Saqlain Saleem</b> <br/>
       </p1>
-      <p2> This my First App for Bootcamp-2020 class 2 practice.</p2>
+      <p2> This my First App for Bootcamp-2020 class 2 practices.</p2>
     </div>
   );
 }
