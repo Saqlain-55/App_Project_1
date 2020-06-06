f6b38f06bc2ab506e110840feea8188f568ac656
Saqlain-55 <Saqlain-55@.github.com>
2020-06-06 17:15:13 +0500

    Initial commit

---
+++ my-react-app/src/App.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div>
+      <h1>Hellow world</h1>
+      <h2>
+        <img src="mypic.jpeg" alt="Designers Hub" hight="10" width="10" allign="left"/>
+      </h2>
+      <h3> this is my first react app for Bootcamp-2020 class 2</h3>
+    </div>
+  );
+}
+
+export default App;
