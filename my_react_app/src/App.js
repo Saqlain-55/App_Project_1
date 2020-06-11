eeb7b5a9ac583378ec53202211519fc9640c365f
Saqlain-55 <Saqlain-55@.github.com>
2020-06-11 05:58:31 +0500

    Update App.js

--- my_react_app/src/App.js
+++ my_react_app/src/App.js
@@ -5,16 +5,9 @@ import myintro from './myintro.js';
 
 function App() {
   return (
-    <div> <className="App">
+    <div> 
       <myintro />
-        <img src="mypic.jpeg" alt="Saqlain Saleem" hight="50" width="50" allign="left"/>
-        
-      <h2>
-        This is muhammad saqlain Saleem
-      </h2>
-      <p1> Hye! <br/> This is <b>Muhammad Saqlain Saleem</b> <br/>
-      </p1>
-      <p2> This my First App for Bootcamp-2020 class 2 practices.</p2>
+       
     </div>
   );
 }
