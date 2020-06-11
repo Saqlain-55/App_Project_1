1af778128781fcc3fce2538555d2dc4d5217f688
Saqlain-55 <Saqlain-55@.github.com>
2020-06-11 06:07:11 +0500

    ubdate app.js file

--- my_react_app/src/App.js
+++ my_react_app/src/App.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
-import './myintro' 
+import myintro from './myintro' 
 
 function App() {
   return (
--- my_react_app/src/myintro.js
+++ my_react_app/src/myintro.js
@@ -1,12 +1,12 @@
 import React from 'react';
 
-function myintro () {
+function myintro (probs) {
     return(
         <div>
         <p1><h1>My Intro</h1></p1>
         <p2> <h2> Hye My name is <b> Muhammad Saqlain Saleem</b></h2></p2>
          <br></br>
-         
+
         </div>
 
     )
