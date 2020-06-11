e786f2954257adbed6320a98f14aaa58f7257dd6
Saqlain-55 <Saqlain-55@.github.com>
2020-06-11 05:45:36 +0500

    Update App.js

--- my_react_app/src/App.js
+++ my_react_app/src/App.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
-import myintro from ' ./myintro.js';
+import myintro from './myintro.js';
 
 function App() {
   return (
